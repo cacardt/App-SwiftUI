77cb90abde2dd83774eb834ee673611ef391c13d
cacardt <theo.cacard@etu.univ-savoie.fr>
2024-01-19 10:34:01 +0100

    Initial Commit

---
+++ App SwiftUI/App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_SwiftUIApp.swift
+//  App SwiftUI
+//
+//  Created by Theo CACARD on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
